a845de0c82a2d13d5d651758eb5a6fb7eec7dffc
julien-leveau <julien-leveau@debian-Bruno>
2014-04-18 15:06:11 +0200

    debut localStorage (chercher pour les include)

--- www/js/app.js
+++ www/js/app.js
@@ -6,18 +6,25 @@ function App(){
 	var REFRESH_TIME=500;
 	var lat,lon;
 	
+	
+	var id=-1;
+	var t=new Array("Date","Latitude","Longitude");
+	
 	this.run=function(){
-		var geoloc_interval = setInterval(getLocation,REFRESH_TIME);
+		var geoloc_interval = setInterval(location,REFRESH_TIME);
 	};
 	
 	function location(){
 		getLocation();
+		storeLocation();
 		printLocation();
 	}
 	
 	function getLocation(){
+		
 		if (navigator.geolocation){
 	      navigator.geolocation.getCurrentPosition(function(position){
+	      	
 		 	  lat = position.coords.latitude ; 
 		 	  lon = position.coords.longitude ;
 		 	  
@@ -29,7 +36,17 @@ function App(){
 	}
 		
 	function printLocation(){
-		//document.getElementById('affichage').innerHTML = "Latitude = " + lat + "<br> Longitude = "+ lon ;
+		document.getElementById('affichage').innerHTML 
+			= "Latitude = " + window.localStorage.getItem(id).getLat();
+			+ "<br> Longitude = "+ window.localStorage.getItem(id).getLon();
+			+ "<br> Date = "+ window.localStorage.getItem(id).getDate();
+	}
+	
+	function storeLocation(){
+		s=new Storage();
+		s.create(lat,lon,date);
+		id++;
+		window.localStorage.set(id,s);
 	}
 }
 

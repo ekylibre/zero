62035aae0c70074a50f821271e1216c17e9bac45
julien-leveau <julien-leveau@debian-Bruno>
2014-04-25 10:57:58 +0200

    current_button suppr dropTable suppr

--- www/js/app.js
+++ www/js/app.js
@@ -2,9 +2,9 @@ function App() {
 
 	var db = null;
 
-	var current_point;
 	var REFRESH_TIME = 1000;
 	var geoloc_interval = null;
+	var running;
 
 	var optionsLoc = {
 		enableHighAccuracy : true,
@@ -12,81 +12,83 @@ function App() {
 		maximumAge : 0
 	};
 
-	 this.initialize=function(){
+	this.initialize = function() {
 		if (db == null) {
 			db = new Database();
 			db.create();
-		}
-		else
+		} else
 			console.log("Database already initialized");
 	};
 
+	///////////////////////////////////////
 	function run() {
-		if (geoloc_interval == null)
+		if (geoloc_interval == null) {
 			geoloc_interval = setInterval(location, REFRESH_TIME);
+			running = true;
+		}
 	}
 
 	function stop() {
+		running = false;
 		clearInterval(geoloc_interval);
 		geoloc_interval = null;
 	}
 
+	/////////////////////////////////////
+
 	//Fonction principale appellée tous les REFRESH_TIME
 	function location() {
-		getLocation();
-		current_point.type = "point";
-		db.storePoint(current_point);
+		navigator.geolocation.getCurrentPosition(successLoc, errorLoc, optionsLoc);
 	}
 
-	//Recuperation des coordonnées
-	function successLoc(pos) {
-		var crd = pos.coords;
-		current_point = {
+	function createLocPoint(crd, typePoint) {
+		var point = {
 			latitude : crd.latitude,
 			longitude : crd.longitude,
 			accuracy : crd.accuracy, //accuracy in meters
 			date : new Date().toString(),
-			type : "point"
+			type : typePoint
 		};
+		return point;
+	}
+
+	//////////////////////////////////////
+
+	function successLoc(pos) {
+		if (running) {
+			var crd = pos.coords;
+			var point = createLocPoint(crd, "loc_point");
+			db.storePoint(point);
+		}
+
+	};
+
+	function successLocStart(pos) {
+		var crd = pos.coords;
+		var point = createLocPoint(crd, "start_point");
+		db.storePoint(point);
+		run();
+	};
+
+	function successLocEnd(pos) {
+		stop();
+		var crd = pos.coords;
+		var point = createLocPoint(crd, "end_point");
+		db.storePoint(point);
 	};
 
 	function errorLoc(err) {
 		console.warn('ERROR(' + err.code + '): ' + err.message);
 	};
 
-	function getLocation() {
-		navigator.geolocation.getCurrentPosition(successLoc, errorLoc, optionsLoc);
-	};
-
+	///////////////////////////////////
 	this.startIntervention = function() {
-		getLocation();
-		setTimeout(function() {
-			current_point.type = "start";
-			db.storePoint(current_point);
-			run();
-		}, 1000);
+		navigator.geolocation.getCurrentPosition(successLocStart, errorLoc, optionsLoc);
 	};
 
 	this.endIntervention = function() {
-		getLocation();
-		setTimeout(function() {
-			current_point.type = "end";
-			db.storePoint(current_point);
-			stop();
-		}, 1000);
+		navigator.geolocation.getCurrentPosition(successLocEnd, errorLoc, optionsLoc);
 	};
-	/*	function printLocation() {
-	 var size = localStorage.length;
-	 data_string = "";
-
-	 for (var i = 0; i < localStorage.length; i++) {
-	 key = window.localStorage.key(i);
-
-	 var value = JSON.parse(window.localStorage.getItem(key));
-	 data_string += "Latitude = " + value["latitude"] + "<br> Longitude = " + value["longitude"] + "<br> Date = " + value["date"] + "<br> <br>";
-	 }
-	 document.getElementById('affichage').innerHTML = data_string;
-	 }*/
 
 }
 
--- www/js/database.js
+++ www/js/database.js
@@ -2,16 +2,16 @@
 function Database() {
 	var db;
 
-	this.create=function(){
+	this.create = function() {
 		db = openDatabase("database", "1", "database", 65536);
-		dropTablePoints();
+	//	dropTablePoints();
 		createTable();
 	};
-	
-//Creation de la table des points
-	function createTable () {
+
+	//Creation de la table des points
+	function createTable() {
 		var query = "CREATE TABLE IF NOT EXISTS points (id INTEGER PRIMARY KEY AUTOINCREMENT,name, latitude FLOAT, longitude FLOAT , date DATE, accuracy NUMERIC, type TEXT, code TEXT, quantity NUMERIC, unit TEXT)";
-		//Creation de la table POINTS 
+		//Creation de la table POINTS
 		db.transaction(function(tx) {
 			tx.executeSql(query, [], function(tx, result) {
 			});
@@ -23,19 +23,24 @@ function Database() {
 		});
 	}
 
-//On efface la table Point
+	//On efface la table Point
 	function dropTablePoints() {
 		db.transaction(function(tx) {
 			tx.executeSql('DROP TABLE IF EXISTS points');
 		});
 	};
 
-//Ajoute une entrée a partir d'un point {latitude,longitude,date,accuracy}
+	//Ajoute une entrée a partir d'un point {latitude,longitude,date,accuracy}
 	this.storePoint = function(point) {
 		if (point != undefined) {
-			var query = "INSERT INTO points (latitude,longitude,date,accuracy,type) VALUES (?,?,?,?,?);";
+			var query = "INSERT INTO points (name,latitude,longitude,date,accuracy,type,code,quantity,unit) VALUES (?,?,?,?,?,?,?,?,?);";
+
+			for (i in point) {
+				if (point[i] == undefined)
+					point[i] = "";
+			}
 			db.transaction(function(tx) {
-				tx.executeSql(query, [point.latitude, point.longitude, point.date, point.accuracy,point.type], function(tx, result) {
+				tx.executeSql(query, [point.name, point.latitude, point.longitude, point.date, point.accuracy, point.type, point.code, point.quantity, point.unit], function(tx, result) {
 					console.log("Query Success");
 				});
 			}, function(error) {

56c4bd91982083398c0c2d7c52e4c9a3d6f4c9d6
Julien Leveau <julien.leveau@yahoo.com>
2014-05-27 17:18:21 +0200

    edit something,

--- www/js/menu.js
+++ www/js/menu.js
@@ -286,18 +286,12 @@ function onDeviceReady() {
 		});
 
 		$("#buttonCode").click(function() {
-			cordova.plugins.barcodeScanner.scan(
-		     function (result) {
-		     	alert('in');
-		          alert("We got a barcode\n" +
-		                "Result: " + result.text + "\n" +
-		                "Format: " + result.format + "\n" +
-		                "Cancelled: " + result.cancelled);
-		      }, 
-		      function (error) {
-		          alert("Scanning failed: " + error);
-		      }
-		   );
+			cordova.plugins.barcodeScanner = new BarcodeScanner();
+			cordova.plugins.barcodeScanner.scan(function(result) {
+				alert("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled);
+			}, function(error) {
+				alert("Scanning failed: " + error);
+			});
 		});
 	});
 }

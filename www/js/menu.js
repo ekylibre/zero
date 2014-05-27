f5cd82dc6742bdaa1cfa679aa987500841596d7d
Julien Leveau <julien.leveau@yahoo.com>
2014-05-27 16:46:54 +0200

    suppr alert

--- www/js/menu.js
+++ www/js/menu.js
@@ -286,7 +286,6 @@ function onDeviceReady() {
 		});
 
 		$("#buttonCode").click(function() {
-			alert("button pushed");
 			cordova.plugins.barcodeScanner.scan(function(result) {
 				alert("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled);
 			}, function(error) {

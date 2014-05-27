e471e700ad3453a49654595bf0c4fe6822a639f7
julien-leveau <julien-leveau@debian-Bruno>
2014-05-27 11:33:16 +0200

    change include

--- www/js/menu.js
+++ www/js/menu.js
@@ -286,7 +286,7 @@ function onDeviceReady() {
 		});
 
 		$("#buttonCode").click(function() {
-			var scanner=cordova.require("com.phonegap.plugins.BarcodeScanner");
+			var scanner=cordova.require("com.phonegap.plugins.barcodescanner.BarcodeScanner");
 			scanner.scan(function(result) {
 				alert("We got a barcode\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled);
 			}, function(error) {
